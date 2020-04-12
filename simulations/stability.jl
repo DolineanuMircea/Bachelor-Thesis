d237153ce53a4a1f1b171a694572c0443815e13b
Mircea <mircea_dolineanu@outlook.com>
2020-04-12 20:52:48 +0300

    halfway through stability

--- simulations/stability.jl
+++ simulations/stability.jl
@@ -24,6 +24,17 @@ function Imp_Euler(N, Δt, icond)
     return [x, v]
 end
 
+# semi-implicit Euler
+function SImp_Euler(N, Δt, icond)
+    x = [icond[1]]
+    v = [icond[2]]
+    for i in 1:convert(Int64, N/Δt)
+        push!(v, v[i] - x[i]*Δt)
+        push!(x, x[i] + v[i+1]*Δt)
+    end
+    return [x, v]
+end
+
 # trapezoid rule
 function Trapezoid(N, Δt, icond)
     x = [icond[1]]
@@ -49,7 +60,7 @@ end
 
 
 function make_plots()
-        # explicit Euler solution
+    # explicit Euler solution
     sol1 = Exp_Euler(N, Δt, icond)
     p1 = plot(sol1[1], sol1[2], seriestype = :scatter, title = "Explicit Euler")
 
@@ -57,29 +68,36 @@ function make_plots()
     sol2 = Imp_Euler(N, Δt, icond)
     p2 = plot(sol2[1], sol2[2], seriestype = :scatter, title = "Implicit Euler")
 
-    # implicit Euler solution
-    sol3 = Trapezoid(N, Δt, icond)
-    p3 = plot(sol3[1], sol3[2], seriestype = :scatter, title = "Trapezoid")
+    # semi-implicit Euler solution
+    sol3 = SImp_Euler(N, Δt, icond)
+    p3 = plot(sol3[1], sol3[2], seriestype = :scatter,
+    title = "Semi-implicit Euler")
 
-    # implicit Euler solution
-    sol4 = St_Ver(N, Δt, icond)
-    p4 = plot(sol4[1], sol4[2], seriestype = :scatter, title = "Störmer-Verlet")
+    # trapezoid solution
+    sol4 = Trapezoid(N, Δt, icond)
+    p4 = plot(sol4[1], sol4[2], seriestype = :scatter, title = "Trapezoid")
+
+    # Störmer-Verlet solution
+    sol5 = St_Ver(N, Δt, icond)
+    p5 = plot(sol5[1], sol5[2], seriestype = :scatter, title = "Störmer-Verlet")
 
     # real solution
     solrx = [sin(x) for x in 0:Δt:N]
     solrv = [cos(x) for x in 0:Δt:N]
     pr = plot(solrx, solrv, seriestype = :scatter, title = "Analytic solution")
 
-    l = @layout [[a b;c d] e]
-    plot(pr,p1,p2,p3,p4, layout = l, xticks = -2:2, xlims = (-2,2),
-    ylims = (-2,2), xlabel = "x", ylabel = "v", legend = false)
-    plot!(size = (700,350))
+    l = @layout [a b c; d e f]
+    plot(pr,p1,p2,p3,p4,p5, layout = l, xticks = -2:2, xlims = (-2.5,2.5),
+    ylims = (-2.5,2.5), xlabel = "x", ylabel = "v", legend = false)
+    plot!(size = (750,500))
 end
 
 # initial conditions
 icond = [0.0,1.0]
-# number of steps and time-step
-N = 10
-Δt = 0.2
+# number of periods and time-step
+N = 15
+Δt = 0.3
 
-make_plots()
+p = make_plots()
+
+png(p, "stability_plots2")
